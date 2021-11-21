1034a562fd4b43ffdb8386fcc894e890ea67f713
kamax <karlikboroda@gmail.com>
2021-11-22 00:38:38 +0300

    add workflow

--- src/app/config/dev.ts
+++ src/app/config/dev.ts
@@ -2,8 +2,12 @@ const ENV = "dev";
 
 const AUTH_DOMAIN = "auth.dev.hopblox.com";
 
-const COOKIE_STORAGE = "health.dev.hopblox.com";
-const REDIRECT_SIGNIN_URL = "https://health.dev.hopblox.com/dashboard";
-const REDIRECT_SIGNOUT_URL = "https://health.dev.hopblox.com/logout";
+const COOKIE_STORAGE = "localhost";
+const REDIRECT_SIGNIN_URL = "http://localhost:" + 3001 + "/dashboard";
+const REDIRECT_SIGNOUT_URL = "http://localhost:" + 3001 + "/logout";
+
+// const COOKIE_STORAGE = "health.dev.hopblox.com";
+// const REDIRECT_SIGNIN_URL = "https://health.dev.hopblox.com/dashboard";
+// const REDIRECT_SIGNOUT_URL = "https://health.dev.hopblox.com/logout";
 
 export { ENV, AUTH_DOMAIN, COOKIE_STORAGE, REDIRECT_SIGNIN_URL, REDIRECT_SIGNOUT_URL };
